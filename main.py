651c105a6474b7312d9e06b07251641b57b98f18
Abdullah Jalal <105108600+LilB1T@users.noreply.github.com>
2024-01-06 06:57:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 
 if __name__ == '__main__':
     subprocess.run(["python3", "detect_and_crop.py", 
-                    "--weights", "yolov7.pt", 
+                    "--weights", "best.pt", 
                     "--source", "vehicle_1-6\\valid\\images\\3_jpg.rf.66aa78b82b9e75754dfbf61170177f7f.jpg", 
                     "--img-size", "640", 
                     "--conf", "0.25"])
