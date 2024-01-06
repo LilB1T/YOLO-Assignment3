04a6bfae348fdda0268db5bad1d33e15aa86a0c3
Abdullah Jalal <105108600+LilB1T@users.noreply.github.com>
2024-01-06 06:47:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,3 @@ if __name__ == '__main__':
                     "--source", "vehicle_1-6\\valid\\images\\3_jpg.rf.66aa78b82b9e75754dfbf61170177f7f.jpg", 
                     "--img-size", "640", 
                     "--conf", "0.25"])
-#weights --> weights file.
-#source  --> validation dataset location.
-#img     --> output image size, must be 640, because the model is trained that way.
-#conf    --> is the confidence threshold, increase to get more fine detections decrease
-           # incase the model doesn't performs well.
